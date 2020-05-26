9a49aa1b2a4cbde53b787d3555ff8259222965d5
Sher6J <liujx17614417627@outlook.com>
2020-05-26 22:49:21 +0800

    update

---
+++ src/cn/sher6j/leetcode/editor/cn/P49GroupAnagrams.java
@@ -0,0 +1,74 @@
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。 
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串
+
+
+package cn.sher6j.leetcode.editor.cn;
+
+import java.util.*;
+
+//Java：字母异位词分组
+public class P49GroupAnagrams{
+    public static void main(String[] args) {
+        Solution solution = new P49GroupAnagrams().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        /**
+         * 当且仅当它们的排序字符串相等时，两个字符串是字母异位词。
+         */
+//        if (strs.length == 0) {
+//            return new ArrayList();
+//        }
+//        HashMap<String, List> map = new HashMap<>();
+//        for (String s : strs)  {
+//            char[] chars = s.toCharArray();
+//            Arrays.sort(chars);
+//            String key = String.valueOf(chars);
+//            if (!map.containsKey(key)) {
+//                map.put(key, new ArrayList());
+//            }
+//            map.get(key).add(s);
+//        }
+//        return new ArrayList(map.values());
+
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
