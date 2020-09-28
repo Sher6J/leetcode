081849ec90863e95699cf4b3faef4575b443fbba
Sher6J <liujx17614417627@outlook.com>
2020-09-28 14:34:32 +0800

    update

---
+++ src/cn/sher6j/leetcode/editor/cn/P4MedianOfTwoSortedArrays.java
@@ -0,0 +1,96 @@
+//给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数。 
+//
+// 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums1 = [1,3], nums2 = [2]
+//输出：2.00000
+//解释：合并数组 = [1,2,3] ，中位数 2
+// 
+//
+// 示例 2： 
+//
+// 输入：nums1 = [1,2], nums2 = [3,4]
+//输出：2.50000
+//解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+// 
+//
+// 示例 3： 
+//
+// 输入：nums1 = [0,0], nums2 = [0,0]
+//输出：0.00000
+// 
+//
+// 示例 4： 
+//
+// 输入：nums1 = [], nums2 = [1]
+//输出：1.00000
+// 
+//
+// 示例 5： 
+//
+// 输入：nums1 = [2], nums2 = []
+//输出：2.00000
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// nums1.length == m 
+// nums2.length == n 
+// 0 <= m <= 1000 
+// 0 <= n <= 1000 
+// 1 <= m + n <= 2000 
+// -106 <= nums1[i], nums2[i] <= 106 
+// 
+// Related Topics 数组 二分查找 分治算法
+
+
+package cn.sher6j.leetcode.editor.cn;
+//Java：寻找两个正序数组的中位数
+public class P4MedianOfTwoSortedArrays{
+    public static void main(String[] args) {
+        Solution solution = new P4MedianOfTwoSortedArrays().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[] merged = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+        if (i == m) {
+            while (j < n) {
+                merged[k++] = nums2[j++];
+
+            }
+        }
+        if (j == n) {
+            while (i < m) {
+                merged[k++] = nums1[i++];
+            }
+        }
+        boolean isSingle = (m + n) % 2 == 0 ? false : true;
+        int mid = (m + n) / 2;
+        if (isSingle) {
+            return merged[mid];
+        } else {
+            return (merged[mid - 1] + merged[mid]) / 2.0;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
