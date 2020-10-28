9d14299f3d3aa085fb8edd218bde4cf4376a3e1b
Sher6J <liujx17614417627@outlook.com>
2020-10-28 09:15:51 +0800

    update

---
+++ src/cn/sher6j/leetcode/editor/cn/P1207UniqueNumberOfOccurrences.java
@@ -0,0 +1,71 @@
+//给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。 
+//
+// 如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：arr = [1,2,2,1,1,3]
+//输出：true
+//解释：在该数组中，1 出现了 3 次，2 出现了 2 次，3 只出现了 1 次。没有两个数的出现次数相同。 
+//
+// 示例 2： 
+//
+// 输入：arr = [1,2]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 输入：arr = [-3,0,1,-3,1,1,1,-3,10,0]
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr.length <= 1000 
+// -1000 <= arr[i] <= 1000 
+// 
+// Related Topics 哈希表 
+// 👍 58 👎 0
+
+
+package cn.sher6j.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//Java：独一无二的出现次数
+public class P1207UniqueNumberOfOccurrences{
+    public static void main(String[] args) {
+        Solution solution = new P1207UniqueNumberOfOccurrences().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        Set<Integer> keys = map.keySet();
+        Set<Integer> vals = new HashSet<>();
+        for (int key:
+             keys) {
+            if (vals.contains(map.get(key))) {
+                return false;
+            }
+            vals.add(map.get(key));
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
