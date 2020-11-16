41382594de85519ca61da03b4da7af79eed0b759
Sher6J <liujx17614417627@outlook.com>
2020-11-16 20:24:37 +0800

    update

---
+++ src/cn/sher6j/leetcode/editor/cn/P74SearchA2dMatrix.java
@@ -0,0 +1,79 @@
+//编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性： 
+//
+// 
+// 每行中的整数从左到右按升序排列。 
+// 每行的第一个整数大于前一行的最后一个整数。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 3
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：matrix = [], target = 0
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 0 <= m, n <= 100 
+// -104 <= matrix[i][j], target <= 104 
+// 
+// Related Topics 数组 二分查找 
+// 👍 274 👎 0
+
+
+package cn.sher6j.leetcode.editor.cn;
+//Java：搜索二维矩阵
+public class P74SearchA2dMatrix{
+    public static void main(String[] args) {
+        Solution solution = new P74SearchA2dMatrix().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) {
+            return false;
+        }
+        int n = matrix[0].length;
+
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid / n][mid % n] == target) {
+                return true;
+            } else if (matrix[mid / n][mid % n] < target) {
+                left = mid + 1;
+            } else if (matrix[mid / n][mid % n] > target) {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
