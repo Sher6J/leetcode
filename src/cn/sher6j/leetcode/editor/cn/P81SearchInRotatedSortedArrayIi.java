2408bf57b713715601c0567697b0e86e0a00d009
Sher6J <liujx17614417627@outlook.com>
2020-11-17 19:42:56 +0800

    update

--- src/cn/sher6j/leetcode/editor/cn/P81SearchInRotatedSortedArrayIi.java
+++ src/cn/sher6j/leetcode/editor/cn/P81SearchInRotatedSortedArrayIi.java
@@ -1,36 +1,40 @@
 //假设按照升序排序的数组在预先未知的某个点上进行了旋转。 
 //
-// ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。 
+// ( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。 
 //
-// 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。 
-//
-// 你可以假设数组中不存在重复的元素。 
-//
-// 你的算法时间复杂度必须是 O(log n) 级别。 
+// 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。 
 //
 // 示例 1: 
 //
-// 输入: nums = [4,5,6,7,0,1,2], target = 0
-//输出: 4
+// 输入: nums = [2,5,6,0,0,1,2], target = 0
+//输出: true
 // 
 //
 // 示例 2: 
 //
-// 输入: nums = [4,5,6,7,0,1,2], target = 3
-//输出: -1 
-// Related Topics 数组 二分查找
+// 输入: nums = [2,5,6,0,0,1,2], target = 3
+//输出: false 
+//
+// 进阶: 
+//
+// 
+// 这是 搜索旋转排序数组 的延伸题目，本题中的 nums 可能包含重复元素。 
+// 这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？ 
+// 
+// Related Topics 数组 二分查找 
+// 👍 246 👎 0
 
 
 package cn.sher6j.leetcode.editor.cn;
-//Java：搜索旋转排序数组
-public class P33SearchInRotatedSortedArray{
+//Java：搜索旋转排序数组 II
+public class P81SearchInRotatedSortedArrayIi{
     public static void main(String[] args) {
-        Solution solution = new P33SearchInRotatedSortedArray().new Solution();
+        Solution solution = new P81SearchInRotatedSortedArrayIi().new Solution();
         // TO TEST
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int search(int[] nums, int target) {
+    public boolean search(int[] nums, int target) {
         int n = nums.length;
         int left = 0, right = n - 1;
 
@@ -38,13 +42,19 @@ class Solution {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                return mid;
+                return true;
+            }
+
+            // skip duplications
+            if (nums[left] == nums[mid]) {
+                left++;
+                continue;
             }
 
             // either nums[left...mid] or nums[mid...right] must be ordered
 
             // nums[left...mid] is ordered
-            if (nums[left] <= nums[mid]) {
+            if (nums[left] < nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else if (target < nums[left] || target > nums[mid]) {
@@ -59,7 +69,7 @@ class Solution {
             }
         }
 
-        return -1;
+        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
