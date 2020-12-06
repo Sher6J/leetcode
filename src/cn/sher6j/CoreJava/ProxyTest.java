acdb0281f4de9964c7c19db57a5141c3bac75320
Sher6J <liujx17614417627@outlook.com>
2020-12-06 12:36:50 +0800

    update

--- src/cn/sher6j/CoreJava/ProxyTest.java
+++ src/cn/sher6j/CoreJava/ProxyTest.java
@@ -12,7 +12,6 @@ import java.util.Random;
  * @create 2020-06-20-14:52
  */
 public class ProxyTest {
-
     public static void main(String[] args) {
 //        Object[] elements = new Object[1000];
 //        for (int i = 0; i < elements.length; i++) {
---
+++ src/cn/sher6j/week/week218/Solution.java
@@ -0,0 +1,110 @@
+package cn.sher6j.week.week218;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ * @author sher6j
+ * @create 2020-12-06-10:26
+ */
+public class Solution {
+
+    public String interpret(String command) {
+        int n = command.length();
+        int idx = 0;
+        StringBuilder res = new StringBuilder();
+        while (idx < n) {
+            if (command.charAt(idx) == 'G') {
+                res.append('G');
+                idx++;
+            } else if (command.charAt(idx) == '(' && command.charAt(idx + 1) == 'a') {
+                res.append("al");
+                idx += 4;
+            } else {
+                res.append("o");
+                idx += 2;
+            }
+        }
+        return res.toString();
+    }
+
+
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        boolean[] isDelete = new boolean[nums.length];
+        int res = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] >= k) {
+                break;
+            }
+            if (isDelete[i] == true) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > k - nums[i]) {
+                    right = mid - 1;
+                } else if (nums[mid] < k - nums[i]) {
+                    left = mid + 1;
+                } else if (nums[mid] == k - nums[i]) {
+                    right = mid - 1;
+                }
+            }
+            if (left >= nums.length || nums[left] != k - nums[i]) {
+                continue;
+            }
+            if (isDelete[left] == false) {
+                isDelete[left] = true;
+                isDelete[i] = true;
+                res++;
+            } else {
+                while (left + 1 < nums.length && nums[left + 1] == nums[left]) {
+                    if (isDelete[left + 1] == false) {
+                        isDelete[left + 1] = true;
+                        isDelete[i] = true;
+                        res++;
+                        break;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == k && !isDelete[i] && !isDelete[j]) {
+//                    isDelete[i] = true;
+//                    isDelete[j] = true;
+//                    res++;
+//                }
+//            }
+        }
+        return res;
+    }
+
+    public int concatenatedBinary(int n) {
+        int num = 2;
+        StringBuilder str = new StringBuilder();
+        str.append(Integer.toBinaryString(1));
+        while (num <= n) {
+            String cur = Integer.toBinaryString(n);
+            long len = cur.length();
+            BigInteger bigInteger = new BigInteger(str.toString());
+        }
+
+        BigInteger bigInteger = new BigInteger(str.toString());
+        bigInteger = bigInteger.divide(new BigInteger("1000000007"));
+        return bigInteger.intValue();
+
+    }
+
+    @Test
+    public void test() {
+        int n = 4;
+        String string = Integer.toBinaryString(n);
+        System.out.println(string);
+        BigInteger num = new BigInteger(string);
+        System.out.println(num.intValue());
+    }
+}
