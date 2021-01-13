8687616999ee56c8bf422f084e6ac68c38c98abf
Sher6J <liujx17614417627@outlook.com>
2021-01-13 22:27:35 +0800

    update

--- src/cn/sher6j/leetcode/editor/cn/P128LongestConsecutiveSequence.java
+++ src/cn/sher6j/leetcode/editor/cn/P128LongestConsecutiveSequence.java
@@ -12,7 +12,9 @@
 
 package cn.sher6j.leetcode.editor.cn;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 //Java：最长连续序列
@@ -24,28 +26,84 @@ public class P128LongestConsecutiveSequence{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
+//        return hashMethod(nums);
+        return ufMethod(nums);
+    }
+
+    private int ufMethod(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-        int longestLength = 0;
-        for (int num : set) {
-            // 跳过所有不是连续数字开头的数组
-            if (!set.contains(num - 1)) {
-                int currNum = num; //当前数字
-                int currLength = 1;
+        UnionFind uf = new UnionFind(nums);
+        for (int num : nums) {
+            uf.union(num, num + 1);
+        }
+        int max = 1;
+        for (int num : nums) {
+            max = Math.max(max, uf.find(num) - num + 1);
+        }
+        return max;
+    }
 
-                while (set.contains(currNum + 1)) {
-                    currNum += 1;
-                    currLength ++;
-                }
+    private class UnionFind {
+        private Map<Integer, Integer> parent;
 
-                longestLength = Math.max(longestLength, currLength);
+        public UnionFind(int[] nums) {
+            int n = nums.length;
+            parent = new HashMap<>(n);
+            for (int i = 0; i < n; i++) {
+                parent.put(nums[i], nums[i]);
             }
         }
-        return longestLength;
+
+        public void union(int x, int y) {
+            Integer rootX = find(x);
+            Integer rootY = find(y);
+            if (rootX == null || rootY == null) {
+                return;
+            }
+            if (rootX.equals(rootY)) {
+                return;
+            }
+            parent.put(rootX, rootY);
+        }
+
+        public Integer find(int x) {
+            if (!parent.containsKey(x)) {
+                return null;
+            }
+            if (x != parent.get(x)) {
+                parent.put(x, find(parent.get(x)));
+            }
+            return parent.get(x);
+        }
+
+
+        private int hashMethod(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int longestLength = 0;
+            for (int num : set) {
+                // 跳过所有不是连续数字开头的数组
+                if (!set.contains(num - 1)) {
+                    int currNum = num; //当前数字
+                    int currLength = 1;
+
+                    while (set.contains(currNum + 1)) {
+                        currNum += 1;
+                        currLength++;
+                    }
+
+                    longestLength = Math.max(longestLength, currLength);
+                }
+            }
+            return longestLength;
+        }
     }
-}
+
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
